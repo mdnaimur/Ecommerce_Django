4825eb43757885742af21efd5a436017e4caa4ea
mdnaimur <mdnaimurr@gmail.com>
2023-09-22 08:56:34 +0600

    Django Ecomrce Project start

---
+++ ecommerce_dajngo/ecommerce_dajngo/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for ecommerce_dajngo project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ecommerce_dajngo.settings')
+
+application = get_asgi_application()
