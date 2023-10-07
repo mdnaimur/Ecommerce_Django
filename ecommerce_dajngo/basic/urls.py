42bd4bb5fa180313c0e5bdd4cbffdd96437df212
mdnaimur <mdnaimurr@gmail.com>
2023-10-07 21:13:20 +0600

    about contact product and database updateed

--- ecommerce_dajngo/basic/urls.py
+++ ecommerce_dajngo/basic/urls.py
@@ -1,6 +1,15 @@
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
 from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-]
+    path('about/', views.about, name='about'),
+    path('contact/', views.contact, name='contact'),
+    path("category/<slug:val>", views.CategoryView.as_view(), name="category"),
+    path("category-title/<val>",
+         views.CategoryTitle.as_view(), name="category-title"),
+    path("product-detail/<int:pk>",
+         views.ProductDetail.as_view(), name="product-detail"),
+]+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
